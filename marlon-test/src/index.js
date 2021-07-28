44290109e2092c9dc56fabe5fe5813c263a8920b
Marlon Dedakis <marlon.dedakis@method4.co.uk>
2021-07-28 17:39:05 +0100

    react testing more

--- marlon-test/src/index.js
+++ marlon-test/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function Button() {
+  var [counter, setCounter] = useState(5);
+  return <button onClick={() => setCounter(counter * 2)}> {counter}</button >;
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Button />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
